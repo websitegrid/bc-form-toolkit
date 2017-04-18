6d2a6d4b842bc83b8eec42219a79cde80687c408
Cory Birdsong <cory@widewebmarketing.com>
2017-04-18 16:14:32 -0500

    Initial commit

---
+++ spec/bc-form-toolkit-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BcFormToolkitView from '../lib/bc-form-toolkit-view';
+
+describe('BcFormToolkitView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
